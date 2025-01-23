a5004bfe69110b3d1a0e3d5d7d2fc53e2707ed9b
Javier Gonzalez <gonzaleztorojavier@gmail.com>
2025-01-23 00:19:20 -0300

    chore(supabase): db setup and created

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,28 +1,10 @@
 import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
-import { vehiclesTable } from './schema'
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 export const client = postgres(process.env.DATABASE_URL ?? '', {
   prepare: false,
 })
-export const db = drizzle(client)
 
-async function main() {
-  const vehicle: typeof vehiclesTable.$inferInsert = {
-    id: '123e4567-e89b-12d3-a456-426614174000',
-    brand: 'Toyota',
-    model: 'Corolla',
-    color: 'Red',
-    year: '2021',
-    version: 'LE',
-    vin: '1234567890',
-    license_plate: 'ABC1234',
-  }
-
-  await db.insert(vehiclesTable).values(vehicle)
-  console.log('New user created!')
-}
-
-main()
+export const db = drizzle(client, { casing: 'snake_case' })
