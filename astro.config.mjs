3e2289652cf02da67b358af61ac8faaa49b4e7c8
Javier Gonzalez <gonzaleztorojavier@gmail.com>
2025-03-31 11:23:41 -0300

    chore(app): tailwind css addended

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import solidJs from "@astrojs/solid-js";
-import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({
-	integrations: [
-		react({ include: ["**/react/*"] }),
-		solidJs({ include: ["**/solid/*"], devtools: true }),
-	],
-	vite: { plugins: [tailwindcss()] },
-});
+export default defineConfig({ vite: { plugins: [tailwindcss()] } });
--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginAstro from "eslint-plugin-astro";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{ files: ["**/*.{js,mjs,cjs,ts}"] },
 	{ languageOptions: { globals: globals.browser } },
+	eslintConfigPrettier,
 	pluginJs.configs.recommended,
+	...eslintPluginAstro.configs.recommended,
 	...tseslint.configs.recommended,
 ];
